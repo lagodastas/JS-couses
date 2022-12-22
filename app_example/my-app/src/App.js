caef2a1ad07b6bed66741de725e8560e72a5bfe7
Stas Lagoda <lagoda.stasik@gmail.com>
2022-12-22 17:48:38 +0200

    Init commit

---
+++ app_example/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
